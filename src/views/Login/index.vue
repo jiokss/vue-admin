ae496a198a9ff80925446ba940d989e6cb69f962
jiokss <372275526@qq.com>
2020-03-27 23:15:13 +0800

    初始化项目

---
+++ src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+   <div id='login'>
+      登录页面
+      <el-button type="primary">主要按钮</el-button>
+      
+   </div>
+</template>
+
+<script>
+export default {
+   name:'login'
+}
+</script>
+<style lang="scss" scoped>
+   #login{
+      height:100vh;
+      background:#344a5f;
+   }
+</style>
